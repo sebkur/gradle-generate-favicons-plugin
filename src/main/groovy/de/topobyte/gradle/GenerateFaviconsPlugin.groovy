9d939a2a49a0a14c2b3f4787caec7ade07d0e064
Sebastian Kürten <sebastian@topobyte.de>
2020-01-13 23:14:14 +0100

    Test file modification date before generating icons

--- src/main/groovy/de/topobyte/gradle/GenerateFaviconsPlugin.groovy
+++ src/main/groovy/de/topobyte/gradle/GenerateFaviconsPlugin.groovy
@@ -4,8 +4,12 @@ import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.InvalidUserDataException
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 class GenerateFaviconsPlugin implements Plugin<Project> {
-    void apply (Project project) {
+    void apply(Project project) {
         def extension = project.extensions.create('favicons', GenerateFaviconsPluginExtension)
         project.task('generateFavicons') {
             group = 'build'
@@ -16,37 +20,45 @@ class GenerateFaviconsPlugin implements Plugin<Project> {
                 }
                 println "Generating favicons from input file ${extension.input}"
 
-                def svg = extension.input
-                def dir = new File(project.buildDir, 'favicons')
-                def subdir = new File(dir, 'images')
-                dir.mkdirs()
-                subdir.mkdirs()
+                def svg = Paths.get(extension.input)
+                def dir = project.buildDir.toPath().resolve('favicons')
+                def subdir = dir.resolve('images')
+                Files.createDirectories(dir)
+                Files.createDirectories(subdir)
                 def out = 'favicon-%d.png'
                 def sizes = [16, 32, 48, 64, 96]
                 for (size in sizes) {
                     def filename = String.format(out, size)
-                    def file = new File(subdir, filename)
-                    println 'Generate icon: ' + file
-                    project.exec {
-                        commandLine = 'inkscape'
-                        args = ['-C', '-h', size, '-e', file.toString(), svg]
+                    def file = subdir.resolve(filename)
+                    if (Files.getLastModifiedTime(file) < Files.getLastModifiedTime(svg)) {
+                        println 'Generate icon: ' + file
+                        project.exec {
+                            commandLine = 'inkscape'
+                            args = ['-C', '-h', size, '-e', file.toString(), svg]
+                        }
+                    } else {
+                        println 'Icon is up to date: ' + file
                     }
                 }
 
-                def icoFile = new File(dir, 'favicon.ico')
-                println 'Generate icon: ' + icoFile
+                def icoFile = dir.resolve('favicon.ico')
 
                 def icoFiles = []
                 def icoSizes = [16, 32, 48, 64]
                 for (size in icoSizes) {
                     def filename = String.format(out, size)
-                    def file = new File(subdir, filename)
+                    def file = subdir.resolve(filename)
                     icoFiles << file.toString()
                 }
-                project.exec {
-                    commandLine = 'convert'
-                    args icoFiles
-                    args icoFile.toString()
+                if (Files.getLastModifiedTime(icoFile) < Files.getLastModifiedTime(svg)) {
+                    project.exec {
+                        println 'Generate icon: ' + icoFile
+                        commandLine = 'convert'
+                        args icoFiles
+                        args icoFile.toString()
+                    }
+                } else {
+                    println 'Icon is up to date: ' + icoFile
                 }
             }
         }
